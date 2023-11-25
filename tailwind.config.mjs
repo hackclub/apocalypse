b453f3c9a6c00a3d16b204acf9fe0727bf2fb848
Sam Liu <hello@samliu.dev>
2023-11-24 21:06:15 -0500

    Add title page

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				"primary-bg": "#401986",
+				"secondary-bg": "#80ABF4",
+				"accent": "#79E2F0"
+			},
+			fontFamily: {
+				"inter": ["'Inter'", "sans-serif"]
+			}
+		},
+	},
+	plugins: [],
+}
