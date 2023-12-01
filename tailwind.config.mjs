bc04a894bd9b37776402b70c270ae9c2088b254e
Mutammim <58490040+mutammim@users.noreply.github.com>
2023-11-30 23:44:02 -0500

    Prettier + IBM Plex Sans + input box

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,20 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				"primary-bg": "#401986",
-				"secondary-bg": "#80ABF4",
-				"accent": "#79E2F0"
-			},
-			fontFamily: {
-				"inter": ["'Inter'", "sans-serif"]
-			},
-			screens: {
-				"xs": "320px"
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-bg": "#401986",
+        "secondary-bg": "#80ABF4",
+        accent: "#79E2F0",
+      },
+      fontFamily: {
+        sans: ["IBM Plex Sans", ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        xs: "320px",
+      },
+    },
+  },
+  plugins: [],
+};
