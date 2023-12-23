06d9b8c9673e42308607a0987a1cbf86244a9e4c
Sam Liu <hello@samliu.dev>
2023-12-23 14:35:11 -0500

    Fix merge conflicts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
+  output: "server",
+  adapter: vercel(),
 });
