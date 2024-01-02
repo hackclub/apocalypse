db34aaf1c43c0eca555a6fd9f5876af86fa6d6c0
Mutammim <58490040+mutammim@users.noreply.github.com>
2024-01-02 18:34:38 -0500

    Vercel Analytics 📈

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,9 @@ import svelte from "@astrojs/svelte";
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
