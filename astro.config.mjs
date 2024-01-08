4ea6f0faf0625a51cd7defcbd876692496c86eff
Arav Narula <arav.1tb@gmail.com>
2024-01-08 15:53:31 -0500

    add redirects to pdf

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
+redirects: {
+    '/prospectus': '/prospectus.pdf'
+  }
   output: "server",
   adapter: vercel({
     webAnalytics: {
