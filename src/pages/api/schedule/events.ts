170f1bf5a4918599a78afbddb4465e4de79ec946
Sam Liu <hello@samliu.dev>
2024-04-23 23:52:48 -0400

    Add schedule api

---
+++ src/pages/api/schedule/events.ts
@@ -0,0 +1,196 @@
+import { createClient } from "@supabase/supabase-js";
+import type { APIContext } from "astro";
+import ical, { type CalendarResponse, type VEvent } from "node-ical";
+
+const supabase = createClient(import.meta.env.SUPABASE_URL, import.meta.env.SUPABASE_SECRET);
+
+type Event = {
+    name: string,
+    location?: string,
+    description?: string,
+    start: Date,
+    end: Date,
+    fullDay: boolean,
+};
+
+type EventResponse = EventsSuccess | EventError;
+
+type EventsSuccess = {
+    ok: true,
+    last_updated: Date,
+    events: Event[],
+};
+
+type EventError = {
+    ok: false,
+    message: string,
+    details?: string,
+};
+
+async function fetchEvents(): Promise<EventResponse> {
+    let schd: CalendarResponse;
+
+    try {
+        schd = await ical.async.fromURL(
+            "https://calendar.google.com/calendar/ical/c_884255a22ba1e092ee65ceead5c91422fe188e21077568270fe5d0633f1927d3%40group.calendar.google.com/public/basic.ics",
+        );
+    } catch (e) {
+        console.error(e);
+        return {
+            ok: false,
+            message: "Failed to fetch calendar",
+            details: e.message,
+        }
+    }
+
+    const events = Object.values(schd).map((event) => {
+        if (event.type !== "VEVENT") {
+            return;
+        }
+        if (!event.start || !event.end || !event.summary) {
+            return;
+        }
+
+        return {
+            name: event.summary,
+            location: event.location,
+            description: event.description,
+            start: event.start,
+            end: event.end,
+            fullDay: event.datetype === "date",
+        };
+    }).filter(event => event);
+
+    return {
+        ok: true,
+        last_updated: new Date(),
+        events: events,
+    };
+}
+
+export async function GET() {
+
+    const { data, error } = await supabase.from("events").select("*").eq("id", "cache");
+    if (error) {
+        console.error("Failed to fetch from supabase, not using cache", error);
+
+        const events = await fetchEvents();
+        if (events.ok == false) {
+            return new Response(
+                JSON.stringify({
+                    ok: false,
+                    message: "Failed to fetch events",
+                    details: events.message,
+                }),
+                {
+                    status: 500,
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Cache-Control": "no-cache, must-revalidate"
+                    },
+                },
+            );
+        }
+
+        const expires = new Date(events.last_updated);
+        expires.setMinutes(expires.getMinutes() + 1);
+
+        return new Response(
+            JSON.stringify(events),
+            {
+                status: 200,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Last-Modified": events.last_updated.toUTCString(),
+                    "Cache-Control": "public, must-revalidate",
+                    "Expires": expires.toUTCString(),
+                },
+            },
+        );
+    }
+
+    if (data.length == 0) {
+        console.warn("No cache found in supabase, fetching events");
+        const events = await fetchEvents();
+        if (events.ok == false) {
+            return new Response(
+                JSON.stringify({
+                    ok: false,
+                    message: "Failed to fetch events",
+                    details: events.message,
+                }),
+                {
+                    status: 500,
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Cache-Control": "no-cache, must-revalidate"
+                    },
+                },
+            );
+        }
+
+        await supabase.from("events").upsert({ id: "cache", last_updated: events.last_updated, events: events.events })
+
+        const expires = new Date(events.last_updated);
+        expires.setMinutes(expires.getMinutes() + 1);
+
+        return new Response(
+            JSON.stringify(events),
+            {
+                status: 200,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Last-Modified": events.last_updated.toUTCString(),
+                    "Cache-Control": "public, must-revalidate",
+                    "Expires": expires.toUTCString(),
+                },
+            },
+        );
+    }
+
+    let events = {
+        ok: true,
+        last_updated: new Date(data[0].last_updated),
+        events: data[0].events,
+    } as EventsSuccess;
+
+    if (events.last_updated.getTime() + 60000 <= Date.now()) {
+        const newEvents = await fetchEvents();
+        if (newEvents.ok == false) {
+            return new Response(
+                JSON.stringify({
+                    ok: false,
+                    message: "Failed to fetch events",
+                    details: newEvents.message,
+                }),
+                {
+                    status: 500,
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Cache-Control": "no-cache, must-revalidate"
+                    },
+                },
+            );
+        }
+
+        await supabase.from("events").upsert({ id: "cache", last_updated: newEvents.last_updated, events: newEvents.events })
+
+        events = newEvents;
+    }
+
+    const expires = new Date(events.last_updated);
+    expires.setMinutes(expires.getMinutes() + 1);
+
+    return new Response(
+        JSON.stringify(events),
+        {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+                "Last-Modified": events.last_updated.toUTCString(),
+                "Cache-Control": "public, must-revalidate",
+                "Expires": expires.toUTCString(),
+            },
+        },
+    );
+}
