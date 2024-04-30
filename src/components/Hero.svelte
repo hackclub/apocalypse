6a6dc5a1b06be2766a1a3bcf6ba7991dc009a17e
Mutammim <58490040+mutammim@users.noreply.github.com>
2024-04-29 20:59:49 -0400

    Add links

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -33,12 +33,20 @@
         <h2 class="font-sans text-base sm:text-lg 3xl:text-2xl">
           May 17-19 @ Shopify Toronto
         </h2>
-        <a
-          href="https://join.apocalypse.hackclub.com"
-          target="_blank"
-          class="inline-block px-12 py-1 font-sans text-base bg-accent2 sm:px-10 3xl:px-16 sm:text-lg 3xl:text-2xl pixel-corners motion-safe:hover:scale-105 motion-safe:hover:shadow-md motion-safe:transition-all motion-safe:active:scale-100"
-          >Register now!</a
-        >
+        <div class="flex flex-col sm:flex-row gap-4 justify-center items-center">
+          <a
+            href="https://join.apocalypse.hackclub.com"
+            target="_blank"
+            class="inline-block px-12 py-1 font-sans text-base border-accent2 border-4 bg-accent2 sm:px-10 3xl:px-16 sm:text-lg 3xl:text-2xl pixel-corners motion-safe:hover:scale-105 motion-safe:hover:shadow-md motion-safe:transition-all motion-safe:active:scale-100"
+            >Register now!</a
+          >
+          <a
+            href="/event"
+            target="_blank"
+            class="inline-block px-12 py-1 font-sans border-accent2 border-4 text-white text-base sm:px-10 3xl:px-16 sm:text-lg 3xl:text-2xl pixel-corners motion-safe:hover:scale-105 motion-safe:hover:shadow-md motion-safe:transition-all motion-safe:active:scale-100"
+            >Links</a
+          >
+        </div>
       </div>
     </div>
   </div>
