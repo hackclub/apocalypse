2ff25eaf702216deabb7afdabf1bcd58d78688f9
Mutammim <58490040+mutammim@users.noreply.github.com>
2024-05-03 15:54:46 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ export default defineConfig({
     '/parents': 'https://docs.google.com/document/d/1NQDChsJd9dK4r1EWsVglzjHr0EnR3iW0DV3xMHSUAZ0/edit',
     '/teachers': 'https://docs.google.com/document/d/1NQDChsJd9dK4r1EWsVglzjHr0EnR3iW0DV3xMHSUAZ0/edit',
     '/travel': 'https://docs.google.com/document/d/1oEQobHOqMcHUU5N7X5egDNo6Tfvqsx3bx5h--UBDpAs/edit',
+    '/international': 'https://docs.google.com/document/d/1LuZmVCTGeFPdXYAwqTYx2IHN2d7h4wjuAdhiWNuYJVI/edit?usp=sharing',
     '/id': 'https://docs.google.com/document/d/1ro77tpRs1vpQK39YGgAP4IumkNhx3-6pc41BmvI0UzA/edit?usp=sharing',
     '/grants': 'https://join.apocalypse.hackclub.com/grants'
   },
